3a897e4b236bbd18155142e8f0744cf99e423155
Christina K <christinakconnect@gmail.com>
2022-03-18 10:46:34 -0500

    Update R-wrapper.sh

--- R-wrapper.sh
+++ R-wrapper.sh
@@ -7,6 +7,8 @@ tar -xzf lubridate_R.3.5.tar.gz
 	
 # Set the library location
 export R_LIBS="$PWD/lubridate_R.3.5"
+# set TMPDIR variable
+export TMPDIR=$_CONDOR_SCRATCH_DIR
 	
 # run the R program
 Rscript --no-save hello_world.R
